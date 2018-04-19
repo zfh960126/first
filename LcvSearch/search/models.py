d32b1954192f8827eda152bd45f9a664eb96bc37
Your Name <you@example.com>
2018-04-19 08:29:13 +0800

    1

---
+++ LcvSearch/search/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+
+# Create your models here.
+
+from datetime import datetime
+from elasticsearch_dsl import DocType, Date, Nested, Boolean, \
+    analyzer,Completion, Keyword, Text,Integer
+from elasticsearch_dsl.connections import connections
+from elasticsearch_dsl.analysis import CustomAnalyzer as _CustomAnalyzer
+
+connections.create_connection(hosts=['localhost'])
+
+class CustomerAnalyzer(_CustomAnalyzer):
+        def get_analysis_definition(self):
+            return {}
+
+
+ik_analyzer = CustomerAnalyzer('ik_max_word',filter =['lowercase'])#filter =['lowercase']  忽略大小写
+
+
+
+
+
+class ArticleType(DocType):
+    #伯乐在线文章类型
+    suggest = Completion(analyzer=ik_analyzer)
+    title = Text(analyzer="ik_max_word")
+    create_date = Date()
+    url = Keyword()
+    url_object_id = Keyword()
+    front_image_url = Keyword()
+    front_image_path = Keyword()
+    praise_nums = Integer()
+    comment_nums = Integer()
+    fav_nums = Integer()
+    tags = Text(analyzer="ik_max_word")
+    content = Text(analyzer="ik_max_word")
+
+    class Meta:
+        index = "jobbole"
+        doc_type = "article"
+
+if __name__ == "__main__":
+    ArticleType.init()
